689471a7853965c39b2bbdf54f8f1d945cc75f0b
mikey <704596141@qq.com>
2023-09-14 11:26:52 +0800

    Site updated: 2023-09-14 11:26:49

---
+++ js/weather.js
@@ -0,0 +1,23 @@
+WIDGET = {
+    "CONFIG": {
+      "modules": "01234",
+      "background": "5",
+      "tmpColor": "4A86E8",
+      "tmpSize": "16",
+      "cityColor": "FF9900",
+      "citySize": "16",
+      "aqiColor": "4A86E8",
+      "aqiSize": "16",
+      "weatherIconSize": "24",
+      "alertIconSize": "18",
+      "padding": "0px 0px 0px 0px",
+      "shadow": "0",
+      "language": "auto",
+      "fixed": "true",
+      "vertical": "center",
+      "horizontal": "center",
+      "left": "135",
+      "top": "18",
+      "key": "a4a9d1c8ccbd4261bab9a03c68760f9a"
+    }
+}
